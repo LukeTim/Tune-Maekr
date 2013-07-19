863cf96fa9f91592e0915ef4aec5ada21136ef87
Luke Timothy <luke.timothy@gmail.com>
2013-07-19 14:01:28 +0100

    Added header and title
    For the sake of checking it's worked once I initially clone this to the webserver.

--- view.php
+++ view.php
@@ -1,7 +1,11 @@
 <html>
   <head>
+    <title>Tune Maekr</title>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.min.js" type="text/javascript"></script>
   </head>
   <body>
+    <div class="header">
+      <h1>Tune Maekr</h1> v 0.1
+    </div>
   </body>
 </html>
